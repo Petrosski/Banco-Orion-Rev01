82c353ed31210df6f357eafe6a039fff3204dc6c
Lucas Oening <105363692+Petrosski@users.noreply.github.com>
2022-10-07 19:03:35 -0300

    Add files via upload

---
+++ Projeto_karen/src/Auxiliar.java
@@ -0,0 +1,26 @@
+public class Auxiliar {
+	public void menuUser() {
+		System.out.println("0 - Sair");
+        System.out.println("1 - Depositar");
+        System.out.println("2 - Sacar");
+        System.out.println("3 - Transferir");
+        System.out.println("4 - Visualizar Saldo");
+        System.out.println("5 - Configurações");
+        System.out.println("===============================");
+        
+	}
+	
+	public void menuLogar(){
+		System.out.println("0 - Sair");
+		System.out.println("1 - Criar conta");
+        System.out.println("2 - Logar");
+	}
+	
+	public void configuracao() {
+		System.out.println("0 - Sair");
+		System.out.println("1 - Alterar Nome");
+        System.out.println("2 - Visualizar Limite");
+        System.out.println("3 - Alterar Renda");
+	}
+	
+}
